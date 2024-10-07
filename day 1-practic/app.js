7b32d1938dabd99e33a5a528bc8ef375b5775de6
AyushBokde <ayushbokde05@gmail.com>
2024-10-07 14:10:58 +0530

    Add files via upload

---
+++ day 1-practic/app.js
@@ -0,0 +1,15 @@
+let id = prompt("Enter Your Id")
+let ps = prompt("Enter Your Password")
+
+
+
+let title = document.querySelector('.lg')
+let btn = document.querySelector('.butt')
+
+btn.addEventListener('click', function(){
+    title.innerHTML = "Login Successfully❤️"
+     btn.innerHTML = "Continue.."
+    
+    })
+
+    
